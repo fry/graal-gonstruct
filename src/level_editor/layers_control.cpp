4a2e0c126360d8dbc423130b7591183e5af13c89
Eike Siewertsen <e.siew@londeroth.org>
2009-09-20 20:46:12 +0200

    Added missing layers_control source files

---
+++ src/level_editor/layers_control.cpp
@@ -0,0 +1,83 @@
+#include "layers_control.hpp"
+#include "window.hpp"
+
+using namespace Graal;
+
+level_editor::layers_control::layers_control(window& win, preferences& prefs)
+: Gtk::HBox(false, 4), m_window(win), m_preferences(prefs),
+  m_button_new_layer("+"), m_button_delete_layer("-") {
+  // Layer selection
+  m_spin_layer.signal_changed().connect_notify(
+    sigc::mem_fun(this, &layers_control::on_layer_changed));
+
+  Gtk::Label& layer_label = *Gtk::manage(new Gtk::Label("Layer:"));
+  pack_start(layer_label, Gtk::PACK_SHRINK);
+  pack_start(m_spin_layer, Gtk::PACK_SHRINK);
+
+  //Gtk::Label& layer_visiblity_label = *Gtk::manage(new Gtk::Label("Visible:"));
+  //pack_start(layer_visiblity_label, Gtk::PACK_SHRINK);
+
+  m_layer_visible.signal_toggled().connect_notify(
+    sigc::mem_fun(this, &layers_control::on_layer_visible_toggled));
+
+  pack_start(m_layer_visible, Gtk::PACK_SHRINK);
+
+  m_button_new_layer.signal_clicked().connect_notify(
+    sigc::mem_fun(this, &layers_control::on_add_layer));
+  pack_start(m_button_new_layer, Gtk::PACK_SHRINK);
+
+  m_button_delete_layer.signal_clicked().connect_notify(
+    sigc::mem_fun(this, &layers_control::on_delete_layer));
+  pack_start(m_button_delete_layer, Gtk::PACK_SHRINK);
+
+
+  win.signal_switch_level_display().connect(
+    sigc::mem_fun(this, &layers_control::on_switch_level_display));
+}
+
+void level_editor::layers_control::on_layer_changed() {
+  level_display* display = m_window.get_current_level_display();
+  int layer = m_spin_layer.get_value_as_int();
+
+  display->set_active_layer(layer);
+
+  m_layer_visible.set_active(display->get_layer_visibility(layer));
+}
+
+void level_editor::layers_control::on_switch_level_display(level_display& display) {
+  int layer_count = display.get_level()->get_layer_count();
+  m_spin_layer.set_range(0, layer_count - 1);
+  //m_spin_layer.set_value(0);
+  m_spin_layer.set_increments(1, 1);
+
+  int layer = m_spin_layer.get_value_as_int();
+  m_layer_visible.set_active(display.get_layer_visibility(layer));
+
+  m_button_delete_layer.set_sensitive(layer_count > 1);
+}
+
+void level_editor::layers_control::on_layer_visible_toggled() {
+  int layer = m_spin_layer.get_value_as_int();
+  m_window.get_current_level_display()->set_layer_visibility(layer, m_layer_visible.get_active());
+}
+
+void level_editor::layers_control::on_add_layer() {
+  int layer = m_spin_layer.get_value_as_int();
+  level_display& display = *m_window.get_current_level_display();
+  display.get_level()->insert_layer(layer + 1);
+  display.set_layer_visibility(layer + 1, true);
+  display.update_all();
+
+  m_spin_layer.set_value(layer + 1);
+  on_switch_level_display(display);
+}
+
+void level_editor::layers_control::on_delete_layer() {
+  int layer = m_spin_layer.get_value_as_int();
+  level_display& display = *m_window.get_current_level_display();
+
+  m_spin_layer.set_value(layer - 1);
+  display.get_level()->delete_layer(layer);
+  on_switch_level_display(display);
+  display.update_all();
+}
---
+++ src/level_editor/layers_control.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <gtkmm.h>
+#include "preferences.hpp"
+#include "level_display.hpp"
+
+namespace Graal {
+  namespace level_editor {
+    class window;
+
+    class layers_control: public Gtk::HBox {
+    public:
+      // TODO: window is currently required to be initialized on construction
+      layers_control(window& win, preferences& prefs);
+    protected:
+      void on_layer_changed();
+      void on_switch_level_display(level_display& display);
+      void on_layer_visible_toggled();
+      void on_add_layer();
+      void on_delete_layer();
+
+      preferences& m_preferences;
+      window& m_window;
+
+      Gtk::SpinButton m_spin_layer;
+      Gtk::CheckButton m_layer_visible;
+      Gtk::Button m_button_new_layer, m_button_delete_layer;
+    };
+  }
+}
