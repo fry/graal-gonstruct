4666043cadbae8fde2316af1a8ea6596f19439b3
Eike Siewertsen <e.siew@londeroth.org>
2010-03-06 13:20:20 +0100

    Remove obsolete and deprecated premake scripts

--- build.sh
+++
@@ -1,4 +0,0 @@
-premake4 gmake || exit
-cd build || exit
-make gonstruct -j 3 || exit
-cd .. || exit
--- build_release.sh
+++
@@ -1,6 +0,0 @@
-premake4 embed || exit
-premake4 gmake || exit
-cd build || exit
-make gonstruct config=release -j 3 || exit
-cd .. || exit
-premake4 dist || exit
