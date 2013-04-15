bcf98292e44331515a7cedc2f6cadacada0eadfd
Benjamin Herr <ben@0x539.de>
2013-04-15 13:56:54 +0200

    make_images.rb: try to support ruby <1.9 and >=1.9

--- src/level_editor/make_images.rb
+++ src/level_editor/make_images.rb
@@ -2,6 +2,12 @@ source, target = ARGV[0], ARGV[1]
 
 images = []
 
+if (RUBY_VERSION.split(".").map{|s|s.to_i} <=> [1,9,0]) < 0
+  def ord(s) s[0] end
+else
+  def ord(s) s.ord end
+end
+
 File.open(target, 'w') do |dst|
   dst.puts(<<-END_OF_HEADER)
 namespace Graal {
@@ -9,7 +15,6 @@ namespace Graal {
     namespace image_data {
   END_OF_HEADER
   Dir.glob(File.join(source, '*.png')) do |img_name|
-    #path = File.join(source, img_name)
     path = img_name
     img_name = File.basename(img_name)
     next if File.directory?(path)
@@ -22,7 +27,7 @@ namespace Graal {
       dst.print("      const char #{var_name}[] =")
       while data = img.read(68)
         dst.print "\n        \"#{data.gsub(/[^[:print:]]|\\|"/) { |ch|
-          "\\#{ch[0].to_s(8).rjust(3, '0')}"
+          "\\#{ord(ch).to_s(8).rjust(3, '0')}"
         }}\""
       end
       dst.print(";\n\n")
