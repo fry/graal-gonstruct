97769d64bb0d93173b378e3ec13a2fad490994a5
Benjamin Herr <ben@0x539.de>
2014-10-19 21:39:24 +0200

    Move helper functions in main() to anon. namespace

--- src/level_editor/main.cpp
+++ src/level_editor/main.cpp
@@ -7,45 +7,47 @@
 #include <boost/scoped_ptr.hpp>
 #include <boost/filesystem/convenience.hpp>
 
-// Called AFTER loading
-void set_default_preferences(Graal::level_editor::preferences& prefs) {
-  // Default tileset
-  if (prefs.tilesets.empty()) {
-    prefs.add_tileset("pics1.png", "");
-    prefs.default_tile = 511; // clear grass tile
-  }
-}
-
-void start_editor(int argc, char* argv[],
-                  boost::scoped_ptr<Graal::level_editor::window>& editor,
-                  Graal::level_editor::preferences& prefs) {
-  if (!boost::filesystem::is_directory(prefs.graal_dir)) {
-    Gtk::Main::quit();
-    return;
-  }
-
-  try {
-    editor.reset(new Graal::level_editor::window(prefs));
-  } catch (std::runtime_error& e) {
-    Gtk::MessageDialog dialog("Error", false,
-        Gtk::MESSAGE_ERROR, Gtk::BUTTONS_OK, true);
-    dialog.set_secondary_text(
-        Glib::ustring("Could not start editor: ") + e.what());
-    dialog.run();
-    Gtk::Main::quit();
-    return;
-  }
-
-  // Load passed level arguments
-  if (argc > 1) {
-    for (int i = 1; i < argc; i ++) {
-      if (argv[i][0] != '-')
-        editor->load_level(argv[i]);
+namespace {
+  // Called AFTER loading
+  void set_default_preferences(Graal::level_editor::preferences& prefs) {
+    // Default tileset
+    if (prefs.tilesets.empty()) {
+      prefs.add_tileset("pics1.png", "");
+      prefs.default_tile = 511; // clear grass tile
     }
   }
 
-  editor->signal_hide().connect(sigc::ptr_fun(&Gtk::Main::quit));
-  editor->show_all();
+  void start_editor(int argc, char* argv[],
+                    boost::scoped_ptr<Graal::level_editor::window>& editor,
+                    Graal::level_editor::preferences& prefs) {
+    if (!boost::filesystem::is_directory(prefs.graal_dir)) {
+      Gtk::Main::quit();
+      return;
+    }
+
+    try {
+      editor.reset(new Graal::level_editor::window(prefs));
+    } catch (std::runtime_error& e) {
+      Gtk::MessageDialog dialog("Error", false,
+          Gtk::MESSAGE_ERROR, Gtk::BUTTONS_OK, true);
+      dialog.set_secondary_text(
+          Glib::ustring("Could not start editor: ") + e.what());
+      dialog.run();
+      Gtk::Main::quit();
+      return;
+    }
+
+    // Load passed level arguments
+    if (argc > 1) {
+      for (int i = 1; i < argc; i ++) {
+        if (argv[i][0] != '-')
+          editor->load_level(argv[i]);
+      }
+    }
+
+    editor->signal_hide().connect(sigc::ptr_fun(&Gtk::Main::quit));
+    editor->show_all();
+  }
 }
 
 int main(int argc, char* argv[]) {
